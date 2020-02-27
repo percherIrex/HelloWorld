55a7b9b19b5f7aae3b92925fb05f2851615df2a3
percherIrex <percherIrex@gmail.com>
2020-02-27 17:58:49 +0800

    初次尝试提交到github

---
+++ HelloGit/src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("version: 3");
+        int result = myUntil.calculate.add(1,1);
+        System.out.println("result = "+result);
+    }
+}
---
+++ HelloGit/src/myUntil/calculate.java
@@ -0,0 +1,7 @@
+package myUntil;
+
+public class calculate {
+    public static int add(int x,int y){
+        return x+y;
+    }
+}
